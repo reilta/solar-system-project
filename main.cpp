903f7af5a5173bb6b0f2cdd55920d4e9c65b1fbd
Igor Dias <igorverissimo02@hotmail.com>
2022-02-03 09:59:50 -0300

    add movimentação

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#include <iostream>
 
-static int year = 0, day = 0;
+using namespace std;
+static int year = 0, day = 0,eixoX=0,eixoZ=0;
 
 void init(void)
 {
@@ -13,7 +15,7 @@ void init(void)
 void planet(float velocity, float dimension, float distance)
 {
     glPushMatrix();
-    glRotatef ((GLfloat) year*(164.80/velocity), 0.0, 1.0, 0.0);
+    glRotatef ((GLfloat) year*(velocity), 0.0, 1.0, 0.0);
     glTranslatef (distance, 0.0, 0.0);
     glRotatef ((GLfloat) day, 0.0, 1.0, 0.0);
 
@@ -23,27 +25,36 @@ void planet(float velocity, float dimension, float distance)
 
 void display(void)
 {
+
+  //  gluLookAt (0.0, 0.0, 15.0, 5.0, 0.0, 0.0, 0.0, 1.0, 0.0);
     glClear (GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
     glColor3f (1.0, 1.0, 1.0);
+    gluLookAt (0, 0.0, 15,0,0 , 0, 0.0, 1.0, 0.0);
+    glTranslatef (eixoX, 0.0, eixoZ);
 
     //sol
+    //gluLookAt (0, 0, 20, 0, eixoX, 0.0, 0.0, 1.0, 0.0);
     glPushMatrix();
     glutWireSphere(1.0, 20, 16);   /* draw sun */
     glPopMatrix();
 
-    planet(0.24,0.1,2); //mercurio
-    planet(0.62,0.25,3); //vênus
+    planet(0.9,0.1,2); //mercurio
+    planet(0.95,0.25,3); //vênus
     glColor3f (0.0, 0.0, 1.0);
     planet(1.0,0.3,4); //terra
     glColor3f (1.0, 1.0, 1.0);
     planet(1.88,0.2,5); //marte
-    planet(11.86,0.6,6.5); //jupiter
-    planet(29.46,0.5,9); //saturno
-    planet(84.01,0.4,11); //urano
-    glColor3f (1.0, 0.0, 0.0);
-    planet(164.80,0.4,13); //netuno
-
+    planet(2,0.6,6.5); //jupiter
+    planet(2.5,0.5,9); //saturno
+    planet(3,0.4,11); //urano
+    glColor3f (2.5, 0.0, 0.0);
+    planet(2,0.4,13); //netuno
+    glPushMatrix();
+    glTranslatef (eixoX, 0.0, 0.0);
+    glPopMatrix();
     glutSwapBuffers();
+
 }
 
 void reshape (int w, int h)
@@ -53,18 +64,37 @@ void reshape (int w, int h)
     glLoadIdentity ();
     gluPerspective(60.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
     glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    gluLookAt (0.0, 0.0, 15.0, 5.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    //glLoadIdentity();
+   // cout<<"entrou"<<endl;
+ //   gluLookAt (0.0, 0.0, 15.0, 5.0, 0.0, 0.0, 0.0, 0.0, 0.0);
 }
 
 void keyboard (unsigned char key, int x, int y)
 {
     switch (key) {
-        case 'd':
+         case 'a':
+            eixoX+=1;
+
+            glutPostRedisplay();
+            break;
+         case 'd':
+            eixoX-=1;
+            glutPostRedisplay();
+            break;
+        case 'w':
+            eixoZ+=1;
+
+            glutPostRedisplay();
+            break;
+        case 's':
+            eixoZ-=1;
+            glutPostRedisplay();
+            break;
+        case 'p':
             day = (day + 10) % 360;
             glutPostRedisplay();
             break;
-        case 'D':
+        case 'P':
             day = (day - 10) % 360;
             glutPostRedisplay();
             break;
@@ -86,7 +116,7 @@ int main(int argc, char** argv)
     glutInit(&argc, argv);
     glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize (500, 500);
-    glutInitWindowPosition (100, 100);
+    glutInitWindowPosition (0, 0);
     glutCreateWindow (argv[0]);
     init ();
     glutDisplayFunc(display);
